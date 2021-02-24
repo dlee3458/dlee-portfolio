cdce8014e125ea74523637fb2c5bfb991db1370a
Daniel Lee <dlee3458@gmail.com>
2021-02-23 18:49:11 -0800

    Configured allowed hosts setting

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('PORTFOLIO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('PORTFOLIO_DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dlee3458.github.io', '127.0.0.1']
 
 
 # Application definition
